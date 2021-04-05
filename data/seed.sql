fac676dd9c25c587a163e5ea34242a53e18f4ce3
reemalqurm <reem.alqurm@gmail.com>
2021-04-06 01:15:04 +0300

    not all is solved

---
+++ data/seed.sql
@@ -0,0 +1,3 @@
+  
+INSERT INTO book (img, bookTitle, authors, book_description, isbn) 
+VALUES('https://target.scene7.com/is/image/Target/GUEST_f23d73cf-1699-4248-a6bf-471ef08a4a65?wid=488&hei=488&fmt=pjpeg', 'Harry Potter Sticker Art Puzzles', 'Reem Alqurm', 'book description 1 goes here.' , '215585');
--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 'use strict';
 
 // Application Dependencies
+require('dotenv').config();
 const express = require('express');
 const superagent = require('superagent');
+const pg = require('pg');
 
 // Application Setup
 const app = express();
+const DATABASE_URL = process.env.DATABASE_URL;
+const client = new pg.Client(DATABASE_URL);
 const PORT = process.env.PORT || 3001 ;
 
 // Application Middleware
@@ -25,10 +29,15 @@ app.get('/searches/new', showForm);
 // Creates a new search to the Google Books API
 app.post('/searches', createSearch);
 
+// app.post('/books', addBook);
+// app.get('/books/:id', getSingleBook);
+
+
 // Catch-all
 app.get('*', (request, response) => response.status(404).send('This route does not exist'));
 
-app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+client.connect().then(()=>{
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));});
 
 // HELPER FUNCTIONS
 // Only show part of this to get students started
@@ -38,16 +47,42 @@ function Book(info) {
   this.img=info.imageLinks|| placeholderImage;
   this.description = info.description|| 'No description available';
   this.authers= info.authers|| 'No authers available';
-
-
+  this.isbn = info.industryIdentifiers[0].identifier || 'No ISBN Available' ;
 
 }
 
 // Note that .ejs file extension is not required
 
 function renderHomePage(request, response) {
-  response.render('pages/index');
+  const sqlString = 'SELECT * FROM book;';
+  client.query(sqlString)
+  .then(result => {
+    response.render('pages/index.ejs', {books: result.rows})
+  }) .catch(internalserverError(response));
+
 }
+function addBook (request, response) {
+  const sqlString = 'INSERT INTO book (img, bookTitle, authors, book_description, isbn) VALUES ($1, $2, $3, $4, $5) RETURNING id;';
+  const sqlArray = [request.body.img, request.body.bookTitle, request.body.authors, request.body.book_description, request.body.isbn];
+  client.query(sqlString, sqlArray)
+  .then(result => {
+    const ejsObject = { book: request.body };
+    response.render('pages/books/detail.ejs', ejsObject);
+  })}
+// function getSingleBook (req, res) {
+//   const sqlString = 'SELECT * FROM book WHERE id=$1;';
+//   const sqlArr = [req.params.id];
+
+//   client.query(sqlString, sqlArr)
+//   .then( result => {
+//     const ejsObject = { books: result.rows[0] };
+//     res.render('pages/books/detail.ejs', ejsObject);
+//   })
+//   .catch(errorThatComesBack => {
+//     res.render('pages/error.ejs');
+//     console.log(errorThatComesBack);
+//   });
+// };
 
 function showForm(request, response) {
   response.render('pages/searches/new.ejs');
@@ -58,8 +93,8 @@ function showForm(request, response) {
 function createSearch(request, response) {
   let url = 'https://www.googleapis.com/books/v1/volumes?q=';
 
-  console.log(request.body);
-  console.log(request.body.search);
+  // console.log(request.body);
+  // console.log(request.body.search);
   
   // can we convert this to ternary?
   if (request.body.search[1] === 'title') { url += `+intitle:${request.body.search[0]}`; }
@@ -72,10 +107,6 @@ function createSearch(request, response) {
 function internalserverError(response){
     return (error)=>{
         console.log(error);
-        res.status(500).send('somthing Went wrong');
+        response.status(500).send('somthing Went wrong');
     }
 }
-
-
-
-
